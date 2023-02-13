a7063087b96f2eb5c83449897900179c9a5fcef6
hoodie <popping2606@gmail.com>
2023-02-13 17:07:49 +0900

    chore: Add base url in vite config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,15 @@
+const { mergeConfig } = require('vite')
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   core: {
     builder: '@storybook/builder-vite',
+  },
+  async viteFinal(config) {
+    // Merge custom configuration into the default config
+    return mergeConfig(config, {
+      base: './',
+    })
   },
 }
